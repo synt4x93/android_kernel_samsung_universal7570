5636b74afa2ac12c2995a2016375412fd2c80967
synt4x93 <synt4x.93@gmail.com>
2021-03-13 02:32:08 +0000

    net: allow CONFIG_KNOX_NCM to be disabled
    Change-Id: I03633ea7ce954001281f5f9eb217412406ae8888

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -114,9 +114,11 @@
 #include <trace/events/skb.h>
 #include <net/busy_poll.h>
 #include "udp_impl.h"
+#if defined(CONFIG_KNOX_NCM)
 /* START_OF_KNOX_NPA */
 #include <net/ncm.h>
 /* END_OF_KNOX_NPA */
+#endif
 
 struct udp_table udp_table __read_mostly;
 EXPORT_SYMBOL(udp_table);
@@ -1778,6 +1780,7 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 	if (sk) {
 		struct dst_entry *dst = skb_dst(skb);
 		int ret;
+		#if defined(CONFIG_KNOX_NCM)
 		/* START_OF_KNOX_NPA */
 		struct nf_conn *ct = NULL;
 		enum ip_conntrack_info ctinfo;
@@ -1785,10 +1788,12 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 		char srcaddr[INET6_ADDRSTRLEN_NAP];
 		char dstaddr[INET6_ADDRSTRLEN_NAP];
 		/* END_OF_KNOX_NPA */
+#endif
 
 		if (unlikely(sk->sk_rx_dst != dst))
 			udp_sk_rx_dst_set(sk, dst);
 
+		#if defined(CONFIG_KNOX_NCM)
 		/* START_OF_KNOX_NPA */
 		/* function to handle open flows with incoming udp packets */
 		if (check_ncm_flag()) {
@@ -1835,12 +1840,13 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 								memcpy(ct->parent_process_name,sk->dns_process_name,sizeof(ct->parent_process_name)-1);
 							}
 							knox_collect_conntrack_data(ct, NCM_FLOW_TYPE_OPEN, 3);
-						}	
+						}
 					}
 				}
 			}
 		}
 		// KNOX NPA - END
+		#endif
 
 		ret = udp_queue_rcv_skb(sk, skb);
 		sock_put(sk);
@@ -1860,6 +1866,7 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 
 	if (sk != NULL) {
 		int ret;
+		#if defined(CONFIG_KNOX_NCM)
 		/* START_OF_KNOX_NPA */
 		struct nf_conn *ct = NULL;
 		enum ip_conntrack_info ctinfo;
@@ -1867,11 +1874,13 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 		char srcaddr[INET6_ADDRSTRLEN_NAP];
 		char dstaddr[INET6_ADDRSTRLEN_NAP];
 		/* END_OF_KNOX_NPA */
+#endif
 
 		if (udp_sk(sk)->convert_csum && uh->check && !IS_UDPLITE(sk))
 			skb_checksum_try_convert(skb, IPPROTO_UDP, uh->check,
 						 inet_compute_pseudo);
 
+		#if defined(CONFIG_KNOX_NCM)
 		/* START_OF_KNOX_NPA */
 		/* function to handle open flows with incoming udp packets */
 		if (check_ncm_flag()) {
@@ -1918,12 +1927,13 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 								memcpy(ct->parent_process_name,sk->dns_process_name,sizeof(ct->parent_process_name)-1);
 							}
 							knox_collect_conntrack_data(ct, NCM_FLOW_TYPE_OPEN, 4);
-						}	
+						}
 					}
 				}
 			}
 		}
 		// KNOX NPA - END
+		#endif
 
 		ret = udp_queue_rcv_skb(sk, skb);
 		sock_put(sk);
